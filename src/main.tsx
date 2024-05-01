90e44a8f977e0077c34fb8401cd444dcb11aff7c
Rouge script <hamzasuleiman7xa7@gmail.com>
2024-05-01 21:42:38 +0100

    goo

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import '../src/index.css'
 import { Provider } from 'react-redux'
 import { store } from './auth/redux/store.tsx'
 
